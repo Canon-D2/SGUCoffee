09b889b6d3a7a70ffc45ab4d83e4f113b957af08
Canon-D2 <chibaodang2002@gmail.com>
2024-03-17 22:19:44 +0700

    Start program Spring Boot

---
+++ src/main/java/com/sgu/SguCoffeeApplication.java
@@ -0,0 +1,14 @@
+package com.sgu;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SguCoffeeApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SguCoffeeApplication.class, args);
+	}
+}
