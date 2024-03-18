79028ae51f6a1018f756b1af38a572b1d4a4f478
Kudo69 <kudoshinichi2804@gmail.com>
2024-03-18 12:40:56 +0700

    Add models

--- src/main/java/com/sgu/model/CartDto.java
+++ src/main/java/com/sgu/model/CartDto.java
@@ -2,9 +2,9 @@ package com.sgu.model;
 
 import lombok.Data;
 @Data
-public class Order_ItemDto{
+public class CartDto {
 	private int id;
 	private int count;
+//	private UserDto user;
 	private ProductDto product;
-//	private OrderDto order;
 }
