b750eef7dbd46ae250f2a15f317023eefc547f54
Canon-D2 <114900012+Canon-D2@users.noreply.github.com>
2024-03-25 15:47:38 +0700

    Update SguCoffeeApplication.java

--- src/main/java/com/sgu/SguCoffeeApplication.java
+++ src/main/java/com/sgu/SguCoffeeApplication.java
@@ -10,5 +10,12 @@ public class SguCoffeeApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SguCoffeeApplication.class, args);
+		System.out.println("-----------------------------------------------------------");
+        System.out.println("🚀 Server web: http://localhost:8080");
+        System.out.println("🚀 Api docs: http://localhost:8080/swagger-ui.html ");
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
 	}
 }
